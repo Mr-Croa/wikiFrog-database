ef583a85c260ff1120bf719314024b45919e1d3d
MrCroa <jules.houdu@gmail.com>
2026-05-20 19:35:12 +0200

    Expand frog text fields

---
+++ migrations/V4__expand_frog_text_fields.sql
@@ -0,0 +1,5 @@
+alter table frogs
+    alter column common_name type text,
+    alter column scientific_name type text,
+    alter column family type text,
+    alter column conservation_status type text;
